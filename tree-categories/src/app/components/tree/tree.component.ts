bc636406bb62102f12fc7c7fe18bd6fa124421f0
Lucas-OrbitaDigital <lucasorbitadigital@gmail.com>
2026-02-20 11:39:05 +0100

    cambio en la funcion NestedTreeControl, que esta deprecada en angular 21

--- tree-categories/src/app/components/tree/tree.component.ts
+++ tree-categories/src/app/components/tree/tree.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { MatTreeNestedDataSource, MatTreeModule } from '@angular/material/tree';
-import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { TreeService } from '../../services/tree.service';
@@ -17,21 +16,28 @@ import { Tree } from '../../models/tree.model';
 export class TreeComponent implements OnInit {
   private treeService = inject(TreeService);
 
-  treeControl = new NestedTreeControl<Tree>(node => node.children);
+  childrenAccessor = (node: Tree) => node.children ?? [];
+  
   dataSource = new MatTreeNestedDataSource<Tree>();
-
-  hasChild = (_: number, node: Tree) => !!node.children && node.children.length > 0;
-  loading = signal<boolean>(true);
+  loading = signal(false);
   errorMessage = signal<string | null>(null);
 
+  hasChild = (_: number, node: Tree) => !!node.children && node.children.length > 0;
+
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.loading.set(true);
+    this.errorMessage.set(null);
     this.treeService.getCategories().subscribe({
-      next: (data: Tree[]) => {
+      next: (data) => {
         this.dataSource.data = data;
         this.loading.set(false);
       },
       error: (err) => {
-        this.errorMessage.set(err.message || 'Error de conexión');
+        this.errorMessage.set(err.message);
         this.loading.set(false);
       }
     });
